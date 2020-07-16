0b6d4f49bbe7268b3bc4db6ff4d06143e88343d4
dabrz <66970384+dabrz@users.noreply.github.com>
2020-07-16 10:56:28 -0400

    Finalised equation V3.5
    -Corrected matrix dimensions issue in both files
    -Added error propagation in eta_map

--- Velocity-driven/isolateMaxVisco.m
+++ Velocity-driven/isolateMaxVisco.m
@@ -7,14 +7,18 @@ function [eta_map] = isolateMaxVisco(L,D,Req_map)
 
 if nargin == 3
     if ismatrix(L) && ismatrix(D) && ismatrix(Req_map)
-        eta_map = zeros(1,size(D,2));
+        eta_map = zeros(size(D,2),2);
         sum_dia = 0;
+        error_sum = 0;
         for j = 1:size(D,2)
-            sum_dia = sum_dia+D(j,1)^4;
+            sum_dia = sum_dia+D(1,j)^4;
+            error_sum = error_sum+4*D(1,j)^3;
         end
         for i=1:size(D,2)
-            eta_map(i,1) = pi*Req_map(i,1)*sum_dia/(L(i,1)*128);
-            eta_map(i,2) = P_allowable*dQ(i)/Q(i)^2/1000;
+            eta_map(i,1) = pi*Req_map(i,1)*sum_dia/(L(1,i)*128);
+            eta_map(i,2) = (Req_map(i,2)*sum_dia*pi/128/L(1,i))^2+...
+                (Req_map(i,1)*pi*L(2,i)*sum_dia/(128*L(1,i)^2))^2+...
+                (Req_map(i,1)*pi*error_sum*(D(2,i))/(128*L(1,i)^2))^2;
         end
     end
 end
--- Velocity-driven/isolateReq.m
+++ Velocity-driven/isolateReq.m
@@ -4,9 +4,10 @@ function [Req_map] = isolateReq(P_allowable,Q,dQ)
 %allowable pressure.
 %Creator: David Brzeski
 %Date:16-07-2020
+
 if nargin == 3
     if isnumeric(P_allowable) && isvector(Q) && isvector(dQ)
-        Req_map = zeros(2,length(Q));
+        Req_map = zeros(length(Q),2);
         for i=1:length(Q)
             Req_map(i,1) = P_allowable*Q(i);
             Req_map(i,2) = P_allowable*dQ(i)/Q(i)^2/1000;
