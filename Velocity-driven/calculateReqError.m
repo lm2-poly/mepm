691896a87bafbb67c26790a0b0c12edade0b0263
dabrz <66970384+dabrz@users.noreply.github.com>
2020-12-01 12:00:26 -0500

    Update calculateReqError.m
    Fixed ( )

--- Velocity-driven/calculateReqError.m
+++ Velocity-driven/calculateReqError.m
@@ -6,8 +6,8 @@ dRi = ones(1,alpha);
 sum_terms = zeros(1,alpha);
 
 for i = 1:alpha
-    dRi(i) = (pi/128)*sum(1./Ri)^(-2).*sqrt((D(1,i).^2./(eta(i).*L(1,i))).^4.*(L(1,i).*deta(i)).^2)+((eta(i).*L(2,i)).^2)+...
-        16.*(((eta(i).*L(1,i).*D(2,i)/D(1,i))).^2);
+    dRi(i) = (pi/128)*sum(1./Ri)^(-2).*sqrt(((D(1,i).^2./(eta(i).*L(1,i))).^4.*(L(1,i).*deta(i)).^2)+((eta(i).*L(2,i)).^2)+...
+        16.*((eta(i).*L(1,i).*D(2,i)/D(1,i))).^2);
     sum_terms(i) = (dRi(i)/Ri(i)^2)^2;
 end
 
